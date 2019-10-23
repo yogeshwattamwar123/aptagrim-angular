0a603849be1889b7d497a5979c3b603d12ddaa90
yogeshwattamwar123 <52085907+yogeshwattamwar123@users.noreply.github.com>
2019-10-23 23:17:09 +0530

    Update apiconnection.js

--- src/app/serverside/apiconnection.js
+++ src/app/serverside/apiconnection.js
@@ -7,7 +7,7 @@ var app = express();
 
 app.use(function (req, res, next) {
 
-  res.header('Access-Control-Allow-Origin', "http://aptagrim-angular.herokuapp.com);
+  res.header('Access-Control-Allow-Origin', "http://aptagrim-angular.herokuapp.com");
   res.header('Access-Control-Allow-Headers', true);
   res.header('Access-Control-Allow-Credentials', 'Content-Type');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
